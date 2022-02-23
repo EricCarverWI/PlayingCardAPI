d99506ddf2a3b1b28169bbf267a5d74a1727c688
EricCarverWI <ericcarverspam@gmail.com>
2022-02-23 08:31:55 -0600

    lets upload this stuff3

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
-
+//Storage Arrays
     var gameDecksHistory = [];   //tracks all decks used since game load
-    var gameCardHistory = [];    //tracks all new requested cards
-    var handWinHistory = [];
+    var handWinHistory = [];        //tracks all hand statistics for report menu
+//Flags for access
     var flagLiveDeck = false;    //tracks whether a deck has been generated
     var activeHand = false;      //tracks whether hand has been dealt and player is active
     var scoreTabulated = false;  //tracks whether a closed hand had score tracked
-    var flagReportWindowClosed = true; 
-
-    var handCounter = 0;
-    var counterHuman = 0;
-    var counterComputer = 0;
-
+    var flagReportWindowClosed = true;  //tracks current status of report window (open or closed)
+//Score tracker
+    var handCounter = 0;        //tracks total hands played
+    var counterHuman = 0;       //tracks player points
+    var counterComputer = 0;    //tracks computer points
+//DOM elements for Hand Value and Hand Card Contents
     const handHuman = document.getElementById("you");
+    const handHumanCards = document.getElementById("'yourArr");
     const handComputer = document.getElementById("dealer");
-
+    const handComputerCards = document.getElementById("'dealerArr")
+//DOM elements for Results / Info 
     const resultsHuman = document.getElementById("resultHUM");
     const resultsComputer = document.getElementById("resultCOMP");
     const gamesCounter = document.getElementById("COUNT");
-
+        gamesCounter.innerHTML = handCounter;
+//DOM Declaration of Buttons
+    const buttonDeck = document.getElementById("deckButton");
+    const buttonReport = document.getElementById("reportButton");
+//DOM Declaration of DIVs
+    const divReport =  document.getElementById("reportDiv");
     var scoreHuman = document.getElementById("HUMW");
-    scoreHuman.innerText = 0;
+        scoreHuman.innerText = 0;
     var scoreComputer = document.getElementById("COMPW");
-    scoreComputer.innerText = 0;
+        scoreComputer.innerText = 0;
+    var deckReminder = false;
 
 //Generate an API deck and saves WHOLE DECK to Array
     const generateNewDeck = async () => {
             //new object for storage
             let newDeck = {};
-            document.getElementById("deckButton").innerText = 'Obtaining new deck Plz Wait..';
+            buttonDeck.innerText = 'Obtaining new deck Plz Wait..';
             try {
                 const res = await fetch (`https://www.deckofcardsapi.com/api/deck/new/draw/?count=52`);
                 newDeck = await res.json();
                 gameDecksHistory.unshift(newDeck);
                 console.log ("this is the newDeck:", newDeck);
-                document.getElementById("deckButton").innerText = (`SUCCESS: deck#: ${gameDecksHistory[0].deck_id}, cards left: ${gameDecksHistory[0].cards.length} ***`);
+                buttonDeck.innerText = (`SUCCESS: deck#: ${gameDecksHistory[0].deck_id}, cards left: ${gameDecksHistory[0].cards.length} ***`);
                 flagLiveDeck = true;  //Ready to play
+                // document.getElementById("buttonDeal").setAttribute("class", "btn btn-primary btn");
             }catch(err){
                 console.log (err);
             }
@@ -45,32 +54,39 @@
             activeHand = true;
             scoreTabulated = false;
             handCounter +=1;
-            document.getElementById("resultHUM").innerHTML = "";
-            document.getElementById("resultCOMP").innerHTML = "";
-            document.getElementById("COUNT").innerHTML = handCounter;
-            document.getElementById("dealer").value = 0;
-            document.getElementById("you").value = 0;
+            resultsHuman.innerHTML = "";
+            resultsComputer.innerHTML = "";
+            gamesCounter.innerHTML = handCounter;
+            handComputer.value = 0;
+            handHuman.value = 0;
 
             let cardDealerNEWFirst = returnNewCardFromDeck();
-            console.log (`This is after the first dealer card: ${cardDealerNEWFirst.code}`);
+                console.log (`This is after the first dealer card: ${cardDealerNEWFirst.code}`);
             let cardHumanNEWFirst = returnNewCardFromDeck();
-            console.log ("This is after the first human card: ", cardHumanNEWFirst.code);
-
+                console.log ("This is after the first human card: ", cardHumanNEWFirst.code);
             let cardHumanNEWSecond = returnNewCardFromDeck();
-            console.log ("This is after the second human card: ", cardHumanNEWSecond.code);
+                console.log ("This is after the second human card: ", cardHumanNEWSecond.code);
 
-            document.getElementById("dealer").value = returnNewCardValue(cardDealerNEWFirst);                //cardDealerFirst;
-            document.getElementById("'dealerArr").value = (`[${returnNewCardInitial(cardDealerNEWFirst)}${returnNewCardSuit(cardDealerNEWFirst)}]`);   //(`[${cardDealerFirst}]`);
-            document.getElementById("you").value = returnNewCardValue(cardHumanNEWFirst) + returnNewCardValue(cardHumanNEWSecond); //cardHumanFirst + cardHumanSecond;
-            document.getElementById("'yourArr").value = (`[${returnNewCardInitial(cardHumanNEWFirst)}${returnNewCardSuit(cardHumanNEWFirst)}] [${returnNewCardInitial(cardHumanNEWSecond)}${returnNewCardSuit(cardHumanNEWSecond)}]`);
+            handComputer.value = returnNewCardValue(cardDealerNEWFirst);                //cardDealerFirst;
+            handComputerCards.value = (`[${returnNewCardInitial(cardDealerNEWFirst)}${returnNewCardSuit(cardDealerNEWFirst)}]`);   //(`[${cardDealerFirst}]`);
+            handHuman.value = returnNewCardValue(cardHumanNEWFirst) + returnNewCardValue(cardHumanNEWSecond); //cardHumanFirst + cardHumanSecond;
+            handHumanCards.value = (`[${returnNewCardInitial(cardHumanNEWFirst)}${returnNewCardSuit(cardHumanNEWFirst)}] [${returnNewCardInitial(cardHumanNEWSecond)}${returnNewCardSuit(cardHumanNEWSecond)}]`);
 ///////BLACKJACK TEST
-            if (document.getElementById("you").value == "21"){
+            if (handHuman.value == "21"){
                 counterHuman += 1;
                 activeHand = false;
-                document.getElementById("resultHUM").innerHTML = "YOU WIN!";
-                document.getElementById("resultCOMP").innerHTML = "BLACKJACK!!";
+                resultsHuman.innerHTML = "YOU WIN!";
+                resultsComputer.innerHTML = "BLACKJACK!!";
                 scoreTabulated = true;
                 tabulateHandHistory();
+            }
+        } else if (flagLiveDeck === true){
+            if (deckReminder == false){
+                buttonDeck.innerText = (`TOO FEW CARDS REMAINING: deck#: ${gameDecksHistory[0].deck_id}, cards left: ${gameDecksHistory[0].cards.length} ***`);
+                deckReminder = true;
+            } else {
+                buttonDeck.innerText = (`NEED TO RESHUFFLE: deck#: ${gameDecksHistory[0].deck_id}, cards left: ${gameDecksHistory[0].cards.length} ***`);
+                deckReminder = false;
             }
         }
     }
@@ -82,17 +98,17 @@
     
 //checks to see if the next card is an Ace value 11 and would break them - if so reduce ace to Ace value 01
             let newCardValue = returnNewCardValue(humanNewCard);
-            if (newCardValue===11 && Number(document.getElementById("you").value)>10){
+            if (newCardValue===11 && Number(handHuman.value)>10){
                 newCardValue = 1;
             }
     
-            document.getElementById("you").value = Number(document.getElementById("you").value) + newCardValue;  //newCard;
-            document.getElementById("'yourArr").value += (` [${returnNewCardInitial(humanNewCard)}${returnNewCardSuit(humanNewCard)}]`);     //(` [${newCard}]`);
-            if(document.getElementById("you").value > 21){
+            handHuman.value = Number(handHuman.value) + newCardValue;  //newCard;
+            handHumanCards.value += (` [${returnNewCardInitial(humanNewCard)}${returnNewCardSuit(humanNewCard)}]`);     
+            if(handHuman.value > 21){
                 counterComputer += 1;
-                document.getElementById("COMPW").innerHTML = counterComputer;
-                document.getElementById("resultCOMP").innerHTML = "PC WIN";
-                document.getElementById("resultHUM").innerHTML = "YOU BUSTED";
+                scoreComputer.innerHTML = counterComputer;
+                resultsComputer.innerHTML = "PC WIN";
+                resultsHuman.innerHTML = "YOU BUSTED";
                 activeHand = false;
                 scoreTabulated=true;
                 tabulateHandHistory();
@@ -102,17 +118,17 @@
 
     function COMPUTER(){
         if (activeHand===true){
-            while(document.getElementById("dealer").value < 17){
+            while(handComputer.value < 17){
                 let computerNewCard = returnNewCardFromDeck();
                 console.log (`This is after the NEW computer card: ${computerNewCard.code}`);
 
 //checks to see if the next card is an Ace value 11 and would break them - if so reduce ace to Ace value 01
                 let newCardValue = returnNewCardValue(computerNewCard);
-                if (newCardValue===11 && Number(document.getElementById("you").value)>10){
+                if (newCardValue===11 && Number(handHuman.value)>10){
                     newCardValue = 1;
                 }                
-                document.getElementById("dealer").value = Number(document.getElementById("dealer").value) + newCardValue; //newCard;
-                document.getElementById("'dealerArr").value += (` [${returnNewCardInitial(computerNewCard)}${returnNewCardSuit(computerNewCard)}]`);          //(` [${newCard}]`);
+                handComputer.value = Number(handComputer.value) + newCardValue; 
+                handComputerCards.value += (` [${returnNewCardInitial(computerNewCard)}${returnNewCardSuit(computerNewCard)}]`);
             }
         }
         activeHand = false;
@@ -120,32 +136,32 @@
 
     function determineWinner(){
         if (activeHand===false && scoreTabulated===false){
-            var a = parseInt(document.getElementById("you").value);
-            var b = parseInt(document.getElementById("dealer").value);
+            let a = Number(handHuman.value);
+            let b = Number(handComputer.value);
             scoreTabulated = true;
             if(b > 21){
                 counterHuman += 1;
-                document.getElementById("HUMW").innerHTML = counterHuman;
-                document.getElementById("resultHUM").innerHTML = "YOU WIN";
+                scoreHuman.innerHTML = counterHuman;
+                resultsHuman.innerHTML = "YOU WIN";
                 tabulateHandHistory();
                 return;
             } else if(a==b){
                 counterHuman += 0.5;
                 counterComputer += 0.5;
-                document.getElementById("HUMW").innerHTML = counterHuman;
-                document.getElementById("COMPW").innerHTML = counterComputer;
-                document.getElementById("resultHUM").innerHTML = "TIE";
-                document.getElementById("resultCOMP").innerHTML = "TIE";
+                scoreHuman.innerHTML = counterHuman;
+                scoreComputer.innerHTML = counterComputer;
+                resultsHuman.innerHTML = "TIE";
+                resultsComputer.innerHTML = "TIE";
                 tabulateHandHistory();
             } else if(a>b){
                 counterHuman += 1;
-                document.getElementById("HUMW").innerHTML = counterHuman;
-                document.getElementById("resultHUM").innerHTML = "YOU WIN";
+                scoreHuman.innerHTML = counterHuman;
+                resultsHuman.innerHTML = "YOU WIN";
                 tabulateHandHistory();
             } else if(b>a){
                 counterComputer += 1;
-                document.getElementById("COMPW").innerHTML = counterComputer;
-                document.getElementById("resultCOMP").innerHTML = "PC WIN";
+                scoreComputer.innerHTML = counterComputer;
+                resultsComputer.innerHTML = "PC WIN";
                 tabulateHandHistory();
             }
         }
@@ -153,7 +169,7 @@
 
     function returnNewCardFromDeck(){
         let newCardObj = gameDecksHistory[0].cards.pop();
-        document.getElementById("deckButton").innerText = (`IN PLAY: deck#: ${gameDecksHistory[0].deck_id}, cards left: ${gameDecksHistory[0].cards.length} ***`);
+        buttonDeck.innerText = (`IN PLAY: deck#: ${gameDecksHistory[0].deck_id}, cards left: ${gameDecksHistory[0].cards.length} ***`);
         return newCardObj;
     }
 
@@ -209,23 +225,23 @@
 
     function generateReport(){
         if (flagReportWindowClosed == true){
-            document.getElementById("reportDiv").innerText = "";
+            divReport.innerText = "";
 
             handWinHistory.forEach(element => {
-                document.getElementById("reportDiv").innerText += element;
+                divReport.innerText += element;
             });
             
             //"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.";
-            document.getElementById("reportButton").innerText = "click to retract report info!";
+            buttonReport.innerText = "click to retract report info!";
             flagReportWindowClosed = false;
         } else {
-            document.getElementById("reportButton").innerText = "click to expand report info!";
-            document.getElementById("reportDiv").innerText = "";
+            buttonReport.innerText = "click to expand report info!";
+            divReport.innerText = "";
             flagReportWindowClosed = true;
         }
     }
 
     function tabulateHandHistory(){
-        handWinHistory.unshift(`${gameDecksHistory[0].deck_id}> \n\thuman: ${Number(document.getElementById("you").value)}-${document.getElementById("'yourArr").value} \n\t\tvs \n\t\t\tcomputer: ${document.getElementById("dealer").value}-${document.getElementById("'dealerArr").value}\n\n`);
+        handWinHistory.unshift(`${gameDecksHistory[0].deck_id}> \n\thuman: ${Number(handHuman.value)}-${document.getElementById("'yourArr").value} \n\t\tvs \n\t\t\tcomputer: ${handComputer.value}-${handComputerCards.value}\n\n`);
     }
 
